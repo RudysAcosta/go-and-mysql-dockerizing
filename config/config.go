f8c2eb886958dfc2d7e68f41c3b07027c391b35e
Rudys Acosta <ncrousset926@gmail.com>
2024-04-08 22:15:42 -0400

    Create config

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import "os"
+
+// Config structure to store the application configuration
+type Config struct {
+	Port     string
+	Database DatabaseConfig
+}
+
+// DatabaseConfig structure to store database configuration
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	Name     string
+}
+
+// NewConfig creates a new instance of Config with default values
+func NewConfig() *Config {
+	return &Config{
+		Port: os.Getenv("PORT"),
+		Database: DatabaseConfig{
+			Host:     os.Getenv("DB_HOST"),
+			Port:     os.Getenv("DB_PORT"),
+			Username: os.Getenv("DB_USERNAME"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Name:     os.Getenv("DB_NAME"),
+		},
+	}
+}
